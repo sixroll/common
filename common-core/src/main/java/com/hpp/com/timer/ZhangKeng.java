7fbb91a92127596c75a2d8be4784e13ef413e74a
huangpingping2009 <huang2009301500055@163.com>
2016-07-05 16:26:58 +0800

    新增加common-core项目

---
+++ common-core/src/main/java/com/hpp/com/timer/ZhangKeng.java
@@ -0,0 +1,11 @@
+package com.hpp.com.timer;
+
+/**
+ * Created by huangpingping on 2016/7/5 15:59
+ * 这个包里会创建一个检查执行时间的类，会比较常用于获取外部接口 长时间无响应的情况
+ */
+public class ZhangKeng {
+    public static void main(String... args){
+
+    }
+}
